e09b4fca570fcbe08a7148ae49c31a5ba9ac02dd
muhammad nabil <muhammad1nabil@yahoo.com>
2025-03-14 02:19:36 +0200

    creating energy_flow for energy flow prioritization

--- BMS_simulator.py
+++ BMS_simulator.py
@@ -17,5 +17,20 @@ class DUT:
         return self.parameters.get(key, None)
 
 
-def simulate_SB():
-    pass
+def energy_flow(pv_panel_power, load_power, battery_capacity):
+    if pv_panel_power > load_power:
+        if battery_capacity < 1:
+            print("charging battery with (PV production - load consumption)")
+        elif battery_capacity == 1:
+            print("selling PV production - load power to the grid")
+    else:
+        if battery_capacity > 0:
+            print(
+                "discharging battery to provide the difference (load consumption - PV production)"
+            )
+        elif battery_capacity == 0:
+            print(
+                "using the grid to provide the difference (load consumption - PV production)"
+            )
+
+
